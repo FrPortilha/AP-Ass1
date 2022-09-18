57547f28956614f64457df327825a871a77e227f
Francisco Portilha <portilhafr@gmail.com>
2022-09-18 23:29:16 +0200

    finished

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -50,21 +49,28 @@ public class Assignment1 {
     }
 
     /**
-     * This method computes question 1 and prints the result
-     * 
-     * @param input string from text file
+     * This method computes question 1 and prints the result with all possible ordered combinations 
+     * of the given cities. This is done by first sovling all possible combinations of the cities, 
+     * and then printing out all possible permutations for each combination.
+     * @param input string from text file.
      */
     public static void question1(String input) {
         String[] values = input.split("[,]", 0);
-        int nCities = Integer.parseInt(values[0]);
-        int nStops = Integer.parseInt(values[1]);
-        getComb(nCities, nStops, values);
-        //System.out.print(combinations);
+        int N = Integer.parseInt(values[0]);
+        int r = Integer.parseInt(values[1]);
+        List<int[]> combList = new ArrayList<>();
+        doCombs(combList, N - 1, 0, 0, new int[r]);
+        for (int i = 0; i < combList.size(); i++) {
+            String citiesComb = "";
+            for (int j = 0; j < r; j++) {
+                citiesComb = citiesComb + values[2 + combList.get(i)[j]];
+            }
+            permutations(citiesComb, "");
+        }
     }
 
     /**
-     * This method computes question 2 and prints the result
-     * 
+     * This method computes question 2 and prints the result.
      * @param input string from text file
      */
     public static void question2(String input) {
@@ -73,9 +79,8 @@ public class Assignment1 {
     }
 
     /**
-     * This method compUtes question 3 and prints the result
-     * 
-     * @param input string from text file
+     * This method computes question 3 and prints the result, the cheops number.
+     * @param input string from text file.
      */
     public static void question3(String input) {
         int number = Integer.parseInt(input);
@@ -83,132 +88,15 @@ public class Assignment1 {
     }
 
     /**
-     * This method computes question 4 and prints the result
-     * 
-     * @param input string from text file
+     * This method computes question 4 and prints the result, all the possible words from the phone number, 
+     * it ignores 0's, 1's and numbers that are repeated.
+     * @param input string from text file.
      */
     public static void question4(String input) {
-        String words = getWords(input);
-        System.out.println(words);
-    }
-
-    public static void question5(String input) {
-        String[] vowels = { "a", "e", "i", "o", "u" };
-        int totalCount = 0;
-        for (String vowel : vowels) {
-            totalCount += countVowelIn(vowel, input.toLowerCase());
-        }
-        System.out.println(totalCount);
-    }
-
-    /**
-     * This method returns an array of strings with of all possible ordered
-     * combinations of the given cities
-     * 
-     * @param values the array of strings with the number of total cities, number of
-     *               stops and the numbers of the cities
-     * @return combinations the array of strings with the ordered combination of the
-     *         cities names
-     */
-    private static String getComb(int N, int r, String[] values) {
-        List<int[]> combList = new ArrayList<>();
-        doCombs(combList, N - 1, 0, 0, new int[r]);
-        String combString = "";
-        for (int i = 0; i < combList.size(); i++) {
-            String citiesComb = "";
-            for (int j = 0; j < r; j++) {
-                citiesComb = citiesComb + values[2 + combList.get(i)[j]];
-            }
-            permutations(citiesComb, "");
-            combString += citiesComb + ",";
-        }
-        return combString;
-    }
-
-    private static void doCombs(List<int[]> combList, int N, int index, int n, int[] comb) {
-        if (index == comb.length) {
-            //List<Integer> list = new ArrayList<>();
-            //for( int i = 0; i <comb.length;i++){
-            //    list.add(comb[i]);
-            //}
-            //System.out.println("Lists : " + list);
-            //List<int[]> permutations = new ArrayList<>();
-            //permute(list, permutations, new int[comb.length], 0);
-            //combList.addAll(permutations);
-            int[] c = comb.clone();
-            combList.add(c);
-        } else if (n <= N) {
-            comb[index] = n;
-            doCombs(combList, N, index + 1, n + 1, comb);
-            doCombs(combList, N, index, n + 1, comb);
-        }
-    }
-
-    //private static void permute(List<Integer> list, List<int[]> permutations, int[] iter, int index){
-    //    if(list.size() == 0){
-    //        int[] p = iter.clone();
-    //        System.out.println(Arrays.toString(p));
-    //        permutations.add(p);
-    //        return;
-    //    }
-    //    for(int i = 0; i < list.size();i++){
-    //        System.out.println();
-    //        iter[index] = list.get(i);
-    //        list.remove(i);
-    //        permute(list, permutations, iter, index + 1);
-    //        
-    //    }
-    // }
-     private static void permutations(String comb, String iter){
-        if(comb.length()==0){
-            System.out.print(iter+",");
-            return;
-        }
-        for(int i = 0; i< comb.length(); i++){
-            char c = comb.charAt(i);
-            String next = comb.substring(0, i) + comb.substring(i+1);
-            permutations(next, iter + c);
-        }
-     }
-
-    /** 
-     * This method counts how many strings in an array are equal to the first
-     * This is solved by recursion
-     * 
-     * @param types the array of strings to be analysed
-     * @param index the index of the string being analysed and counted
-     * @return the total count of occurances of strings equal to the first
-     */
-    private static int count(String[] types, int index) {
-        if (index == 0)
-            return 0;
-        int c = 0;
-        if (types[0].equals(types[index])) {
-            c = 1;
-        }
-        return c + count(types, index - 1);
-    }
-
-    /**
-     * This method retuns the cheops number of a triangle with side length number
-     * This is solved by recursion
-     * 
-     * @param number the length of the cheops triangle
-     * @return the total number of balls in the cheops triangle
-     */
-    private static int doCheops(int number) {
-        if (number == 1)
-            return 1;
-        return number + doCheops(number - 1);
-    }
-
-    private static String getWords(String input) {
-        //String clean = clean(input);
         String[] numbers = input.split("[,]", 0);
-        int nul = countVowelIn("1", input) + countVowelIn("0", input);
+        int nul = countVowelIn("1", input) + countVowelIn("0", input) + countSame(numbers,0,0);
         int[] choices = options(numbers, nul);
         int[] initComb = initComb(numbers, nul);
-        //System.out.println("Initial comb : " + Arrays.toString(initComb) + " choices : " + Arrays.toString(choices));
         List<int[]> wordList = new ArrayList<>();
         doWords(wordList, choices, 0, 0, initComb, new int[initComb.length]);
         String words = "";
@@ -222,11 +110,93 @@ public class Assignment1 {
                 words += ",";
             }
         }
-        return words;
+        System.out.println(words);
     }
-
+    /**
+     * This method computes question 5 and prints the result, the number of vowels in the input file.
+     * @param input string from text file.
+     */
+    public static void question5(String input) {
+        String[] vowels = { "a", "e", "i", "o", "u" };
+        int totalCount = 0;
+        for (String vowel : vowels) {
+            totalCount += countVowelIn(vowel, input.toLowerCase());
+        }
+        System.out.println(totalCount);
+    }
+    /**
+     * This method computes all the possible combinations of cities represented by arrays of numbers.
+     * This method is solved by recursion.
+     * @param combList the list with the possible combinations.
+     * @param N the number of total cities.
+     * @param index the index of thr element in current combination that is being computed.
+     * @param n the number representing the a city that is being registered in the current combination.
+     * @param comb the current combination.
+     */
+    private static void doCombs(List<int[]> combList, int N, int index, int n, int[] comb) {
+        if (index == comb.length) {
+            int[] c = comb.clone();
+            combList.add(c);
+        } else if (n <= N) {
+            comb[index] = n;
+            doCombs(combList, N, index + 1, n + 1, comb);
+            doCombs(combList, N, index, n + 1, comb);
+        }
+    }
+    /**
+     * This method prints all the possible permutations of a given string. This method is solved by recursion.
+     * @param comb the string to be permutated and printed.
+     * @param iter the current iteration of a permutation to be printed.
+     */
+    private static void permutations(String comb, String iter){
+       if(comb.length()==0){
+           System.out.print(iter+",");
+           return;
+       }
+       for(int i = 0; i< comb.length(); i++){
+           char c = comb.charAt(i);
+           String next = comb.substring(0, i) + comb.substring(i+1);
+           permutations(next, iter + c);
+       }
+    }
+    /** 
+     * This method counts how many strings in an array are equal to the first
+     * This is solved by recursion.
+     * @param types the array of strings to be analysed.
+     * @param index the index of the string being analysed and counted.
+     * @return the total count of occurances of strings equal to the first.
+     */
+    private static int count(String[] types, int index) {
+        if (index == 0)
+            return 0;
+        int c = 0;
+        if (types[0].equals(types[index])) {
+            c = 1;
+        }
+        return c + count(types, index - 1);
+    }
+    /**
+     * This method returns the cheops number of a triangle with side of length number.
+     * This is solved by recursions.
+     * @param number the length of the cheops triangle
+     * @return the total number of balls in the cheops triangle
+     */
+    private static int doCheops(int number) {
+        if (number == 1)
+            return 1;
+        return number + doCheops(number - 1);
+    }
+    /**
+     * This method computes the possible combinations of letters for all words possible from the phone number. 
+     * This method is solved by recursion.
+     * @param wordList list of words that are possible from the phone number.
+     * @param choices array with number of letters corresponding to each number.
+     * @param index index of the digit that is being represented to a possible letter.
+     * @param n number of choices that this index has already computed.
+     * @param initComb the initial combination of the possible words.
+     * @param comb the current combination that is being computed.
+     */
     private static void doWords(List<int[]> wordList, int[] choices, int index, int n, int[] initComb, int[] comb) {
-
         if (index == comb.length) {
             int[] c = comb.clone();
             wordList.add(c);
@@ -236,35 +206,32 @@ public class Assignment1 {
             doWords(wordList, choices, index, n + 1, initComb, comb);
         }
     }
-    //private static String clean(String input){
-    //    String[] unwanted = {"0","1"};
-    //    for (String s : unwanted) {
-    //        while(input.contains(s)){
-    //            input = input.substring(0, input.indexOf(s)) + input.substring(input.indexOf(s)+1);
-    //        }
-    //    }
-    //    return input;
-    //}
+    /**
+     * This method computes the initial combination of letters for the given phone number, it ignores 0's and 1's.
+     * @param numbers of the phonne number to be checked.
+     * @param nul number of 0's and 1's and equal consecutive numbers to ignore.
+     * @return an array with the initial letters (in corresponding numbers).
+     */    
     private static int[] initComb(String[] numbers, int nul) {
         int[] initComb = new int[numbers.length - nul];
         int[] key = new int[] { 0, 0, 1, 4, 7, 10, 13, 16, 20, 23 };
         int index = 0;
         for (int i = 0; i < numbers.length; i++) {
             int n = Integer.parseInt(numbers[i]);
-            if (n != 0 && n != 1) {
-                initComb[index] = key[n];
-                index++;
+            if (n != 0 && n != 1 ){
+                if((i>0 && n!=Integer.parseInt(numbers[i-1]))||i==0){
+                    initComb[index] = key[n];
+                    index++;
+                }
             }
         }
         return initComb;
     }
-
     /**
-     * This method computes how many letters each number corresponds to and returns
-     * it in an array
-     * 
-     * @param numbers of the phonne number to be checked
-     * @return an array with the number of letters each number corresponds to
+     * This method computes how many letters each number corresponds to and returns it in an array.
+     * @param numbers of the phonne number to be checked.
+     * @param nul number of 0's and 1's and equal consecutive numbers to ignore.
+     * @return an array with the number of letters each number corresponds to.
      */
     private static int[] options(String[] numbers, int nul) {
         int[] choices = new int[numbers.length - nul];
@@ -273,18 +240,31 @@ public class Assignment1 {
         for (int i = 0; i < numbers.length; i++) {
             int n = Integer.parseInt(numbers[i]);
             if (n != 0 && n != 1) {
-                choices[index] = key[n];
-                index++;
+                if((i>0 && n!=Integer.parseInt(numbers[i-1]))||i==0){
+                    choices[index] = key[n];
+                    index++;
+                }
             }
         }
         return choices;
     }
-
+    /**
+     * This method counts how many conseccutive numbers are equal. This is solved by recursion.
+     * @param numbers the numbers to be checked.
+     * @param count the count of consecutive equal numbers.
+     * @param index the index of the number being checked.
+     * @return the count of consectutive equal numbers.
+     */
+    private static int countSame(String[] numbers, int count, int index){
+        if(index == numbers.length -1) return count;
+        else if(numbers[index].equals(numbers[index+1])){
+            count++;
+        }
+         return countSame(numbers,count, index + 1);
+    }
     /**
      * This method counts the number of occurences of a specific character in a
-     * string
-     * This is solved by recursion
-     * 
+     * string. This is solved by recursion.
      * @param vowel the specific character to be counted
      * @param input the string to be analysed
      * @return the total count of occurances
